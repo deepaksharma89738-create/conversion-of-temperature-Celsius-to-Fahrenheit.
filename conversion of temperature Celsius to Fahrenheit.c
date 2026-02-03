aba126e7b9a84ddebf8a6c9290999d61485d195a
Deepak Sharma <deepaksharma89738@gmail.com>
2026-02-03 23:07:23 +0530

    Add files via upload

---
+++ conversion of temperature Celsius to Fahrenheit.c
@@ -0,0 +1,11 @@
+/*Program conversion of temperature Celsius to Fahrenheit*/
+#include<stdio.h>
+int main()
+{
+    float c, f;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &c);
+    f = (c * 9/5) + 32;
+    printf("Temperature in Fahrenheit = %f", f);
+    return 0;
+}
